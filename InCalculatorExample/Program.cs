a15d111351cf85f1b6ec64b5815b41fc3b90edc0
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-09-07 10:51:47 +0200

    change the name

---
+++ InCalculatorExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntCalculatorExample
+{
+    public class IntCalculatorProgram
+    {
+        public static void Main()
+        {
+            IntCalculator cal = new();
+            cal.Add(int.MaxValue);
+            Console.WriteLine(cal.Result);
+
+            cal.Add(1);
+            Console.WriteLine(cal.Result);
+        }
+    }
+}
